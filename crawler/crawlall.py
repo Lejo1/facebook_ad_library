b0a248cfdad0305040a4093f8dccb9b9a8920551
Lejo1 <Lejo_1@web.de>
2022-03-27 15:56:01 +0200

    Catch Errors while crawling and retry

--- crawler/crawlall.py
+++ crawler/crawlall.py
@@ -70,85 +70,90 @@ class Crawler(Thread):
         retried = 0
         global _RUN
         while _RUN:
-            print("Running link... After: %s" % self.after)
-            firsturl = config.URL + "?access_token=%s&search_terms=*&ad_reached_countries=%s&ad_active_status=ALL&fields=%s&limit=%i" % (
-                config.TOKENS[self.tround][1], lang, config.FIELDS, limit)
-            if self.after != "":
-                firsturl += "&after=%s" % self.after
+            try:
+                print("Running link... After: %s" % self.after)
+                firsturl = config.URL + "?access_token=%s&search_terms=*&ad_reached_countries=%s&ad_active_status=ALL&fields=%s&limit=%i" % (
+                    config.TOKENS[self.tround][1], lang, config.FIELDS, limit)
+                if self.after != "":
+                    firsturl += "&after=%s" % self.after
 
-            response = requests.get(firsturl)
-            # Checking the usage header:
-            self.cooldown(response.headers)
+                response = requests.get(firsturl)
+                # Checking the usage header:
+                self.cooldown(response.headers)
 
-            if response.status_code != 200:
-                err = "Status: %i Response: %s" % (
-                    response.status_code, response.content)
-                print("Something went wrong... %s" % err)
+                if response.status_code != 200:
+                    err = "Status: %i Response: %s" % (
+                        response.status_code, response.content)
+                    print("Something went wrong... %s" % err)
+                    out = response.json()
+                    if out and "error" in out:
+                        if response.status_code == 500:
+                            if "message" in out["error"]:
+                                if out["error"]["message"] == "Please reduce the amount of data you\'re asking for, then retry your request":
+                                    # We just try it again with half the amount of data
+                                    limit = int(limit * 0.5)
+                                    print(
+                                        "Got too much data, Retring with half the amount(%i)" % limit)
+                                    continue
+
+                        if out["error"]["code"] == 1 or out["error"]["code"] == 2:
+                            # These just occur sometimes and we don't want the whole script to stop.
+                            print("Unexpected error! Watiting 1min...")
+                            time.sleep(60)
+                            continue
+
+                        elif out["error"]["code"] == 190:
+                            # stop the script to prevent error-spaming
+                            print("INVALID Token!")
+                            _RUN = False
+
+                        elif out["error"]["code"] == 613:
+                            # the cooldown function has already switched to the next key so we can just retry
+                            print("Got Rate limited!")
+                            continue
+
+                    # We can't handle this error
+                    return False
+
                 out = response.json()
-                if out and "error" in out:
-                    if response.status_code == 500:
-                        if "message" in out["error"]:
-                            if out["error"]["message"] == "Please reduce the amount of data you\'re asking for, then retry your request":
-                                # We just try it again with half the amount of data
-                                limit = int(limit * 0.5)
-                                print(
-                                    "Got too much data, Retring with half the amount(%i)" % limit)
-                                continue
+                data = out["data"]
+                amount = len(data)
+                if "paging" in out:
+                    # This is the pointer to get results of the next page
+                    # The last one poiting to the next empty page will be saved in the msg field
+                    self.after = out["paging"]["cursors"]["after"]
 
-                    if out["error"]["code"] == 1 or out["error"]["code"] == 2:
-                        # These just occur sometimes and we don't want the whole script to stop.
-                        print("Unexpected error! Watiting 1min...")
-                        time.sleep(60)
-                        continue
+                if amount > 0:
+                    for i, entrie in enumerate(data):
+                        # Data is saved to a mongodb, so we use the primary key _id
+                        data[i]["_id"] = entrie["id"]
+                        del data[i]["id"]
 
-                    elif out["error"]["code"] == 190:
-                        # stop the script to prevent error-spaming
-                        print("INVALID Token!")
-                        _RUN = False
+                    # Create bulk of upsert updates to allow key-duplicates
+                    upserts = [UpdateOne({'_id': x['_id']}, {
+                                         '$set': x}, upsert=True) for x in data]
+                    result = ads.bulk_write(upserts)
 
-                    elif out["error"]["code"] == 613:
-                        # the cooldown function has already switched to the next key so we can just retry
-                        print("Got Rate limited!")
-                        continue
+                    print("Inserted %i items" % amount)
+                    limit = config.LIMIT
 
-                # We can't handle this error
-                return False
+                if not "paging" in out:
+                    print("Got no new paging cursor!")
+                    retried += 1
+                    if retried > 3:
+                        # We retried the same after pointer for 3 times.
+                        # We can be quiet sure it's really the end
+                        break
 
-            out = response.json()
-            data = out["data"]
-            amount = len(data)
-            if "paging" in out:
-                # This is the pointer to get results of the next page
-                # The last one poiting to the next empty page will be saved in the msg field
-                self.after = out["paging"]["cursors"]["after"]
-
-            if amount > 0:
-                for i, entrie in enumerate(data):
-                    # Data is saved to a mongodb, so we use the primary key _id
-                    data[i]["_id"] = entrie["id"]
-                    del data[i]["id"]
-
-                # Create bulk of upsert updates to allow key-duplicates
-                upserts = [UpdateOne({'_id': x['_id']}, {
-                                     '$set': x}, upsert=True) for x in data]
-                result = ads.bulk_write(upserts)
-
-                print("Inserted %i items" % amount)
-                limit = config.LIMIT
-
-            if not "paging" in out:
-                print("Got no new paging cursor!")
-                retried +=1
-                if retried > 3:
-                    # We retried the same after pointer for 3 times.
-                    # We can be quiet sure it's really the end
-                    break
-
-            else:
-                retried = 0
+                else:
+                    retried = 0
+            except Exception as e:
+                print("Unexpected Error while crawling, sleeping 60s Error: %s" % str(e))
+                time.sleep(60)
 
         print("Finished, last Pointer: %s" % self.after)
         return True
+
 
 # Spawn multiple crawling threads
 if __name__ == "__main__":
